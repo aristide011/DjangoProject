40ca63383d42fb6d0e2b824c4dfc1a71f89c64fc
GraDivi004 <aristide.motchoue@edu.unifi.it>
2025-07-16 20:13:26 +0200

    add runtime.txt e shell.nix

--- Back_end/settings.py
+++ Back_end/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Back_end.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-
+        'DIRS': [BASE_DIR/'users/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
