2591e096f0539de0c915a128e0ba972352b17ba5
GraDivi004 <aristide.motchoue@edu.unifi.it>
2025-07-17 01:57:26 +0200

    Pronto per il deploy su railway

--- Back_end/settings.py
+++ Back_end/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Back_end.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
